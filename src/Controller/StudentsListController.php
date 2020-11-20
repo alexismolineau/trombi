ef3bc33d5c0777761a1ddc937d5cbfdfd85c5fd3
Alexis Molineau <alexis@cefimmac109.home>
2020-11-20 17:03:16 +0100

    added elements from DB to index view in Controller

--- src/Controller/StudentsListController.php
+++ src/Controller/StudentsListController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\StudentRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 class StudentsListController extends AbstractController
 {
     /**
-     * @Route("/students/list", name="students_list")
+     * @Route("/", name="students_list")
      */
     public function index(): Response
     {
+        $students = $this->getDoctrine()->getRepository(Student::class)->findAll();
         return $this->render('students_list/index.html.twig', [
             'controller_name' => 'StudentsListController',
+            'students' => $students,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="students")
+     */
+    public function list(int $id): Response
+    {
+        return $this->render('students_list/list.html.twig', [
+            'students' => 'test',
         ]);
     }
 }
