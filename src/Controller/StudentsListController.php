d8e5977a69d756186bd8ab9416fead0c1b3825e3
Alexis Molineau <alexis@cefimmac109.home>
2020-11-22 01:07:23 +0100

    added htpwd to protect the access of the site

--- src/Controller/StudentsListController.php
+++ src/Controller/StudentsListController.php
@@ -6,8 +6,6 @@ use App\Entity\Student;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\StudentRepository;
-use Doctrine\Persistence\ManagerRegistry;
 
 
 class StudentsListController extends AbstractController
@@ -19,7 +17,6 @@ class StudentsListController extends AbstractController
     {
         $students = $this->getDoctrine()->getRepository(Student::class)->findAll();
         return $this->render('students_list/index.html.twig', [
-            'controller_name' => 'StudentsListController',
             'students' => $students,
         ]);
     }
