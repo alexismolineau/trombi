cb3b63aadd4717ae2a314e3dc3859d05879847f4
Alexis Molineau <alexis@cefimmac109.home>
2020-11-29 00:22:49 +0100

    updated promotion JS to sort promotion by year

--- public/assets/js/tri.js
+++ public/assets/js/tri.js
@@ -1,5 +1,7 @@
 const cards = document.querySelectorAll('.card-container');
+const promotionCards = document.querySelectorAll('.promotion-card');
 const content = document.querySelector('.main-content');
+const promotionList = document.querySelector('.promotion-list');
 
 function sortByName(){
     const cardsArray = Array.from(cards);
@@ -17,6 +19,23 @@ function sortByName(){
     return cardsArray;
 }
 
+
+function sortByNumber(){
+    const promotionCardsArray = Array.from(promotionCards);
+    promotionCardsArray.sort(function(a, b){
+        if(a.children[1].children[0].innerHTML == b.children[1].children[0].innerHTML){
+            return 0;
+        }
+        if(a.children[1].children[0].innerHTML < b.children[1].children[0].innerHTML){
+            return -1;
+        }
+        if(a.children[1].children[0].innerHTML > b.children[1].children[0].innerHTML){
+            return 1;
+        }
+    });
+    return promotionCardsArray;
+}
+
 function triAZ(){
     cardsArray = sortByName();
 
@@ -30,5 +49,21 @@ function triZA(){
 
     for(let i = cardsArray.length - 1; i >= 0; i--){
         content.appendChild(cardsArray[i]);
+    }
+}
+
+function tri19(){
+    promotionCardsArray = sortByNumber();
+
+    for(let i = 0; i < promotionCardsArray.length; i++){
+        promotionList.appendChild(promotionCardsArray[i]);
+    }
+}
+
+function tri91(){
+    promotionCardsArray = sortByNumber();
+
+    for(let i = promotionCardsArray.length - 1; i >= 0; i--){
+        promotionList.appendChild(promotionCardsArray[i]);
     }
 }
