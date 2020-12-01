18c2edd3ba2d63cd598c33279e11cc5f8dbd0291
Alexis Molineau <alexis@cefimmac109.home>
2020-12-01 13:59:54 +0100

    added method in admin controller for Student and Promotion

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -40,13 +40,61 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/promotions/{$id}", name="admin_show_promotion")
+     * @Route("/admin/promotions/{id}", name="admin_show_promotion")
      */
     public function showPromotion($id) :Response
     {
         $promotion = $this->getDoctrine()->getRepository(Promiton::class)->findOneBy(['id' => $id]);
 
         return $this->render('showPromotion.html.twig', [
+            'promotion' => $promotion,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/promotions/add", name="admin_add_promotion")
+     */
+    public function addPromotion() :Response
+    {
+        $promotion = new Promotion;
+
+        //TODO form add promotion
+        $form = '';
+
+
+        return $this->render('addPromotion.html.twig', [
+            //'addPromotionForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/promotions/mod/{id}", name="admin_mod_promotion")
+     */
+    public function modPromotion($id) :Response
+    {
+
+        $promotion = $this->getDoctrine()->getRepository(Promotion::class)->findOneBy(['id' => $id]);
+
+        //TODO form mod promotion
+        $form = '';
+
+        return $this->render('modPromotion.html.twig', [
+            //'modPromotionForm' => $form->createView(),
+            'promotion' => $promotion,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/promotions/del/{id}", name="admin_del_promotion")
+     */
+    public function delPromotion($id) :Response
+    {
+        $promotion = $this->getDoctrine()->getRepository(Promotion::class)->findOneBy(['id' => $id]);
+        //TODO form delete promotion``
+        $form = '';
+
+        return $this->render('delPromotion.html.twig', [
+            //'delPromotionForm' => $form->createView(),
             'promotion' => $promotion,
         ]);
     }
@@ -64,8 +112,61 @@ class AdminController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/admin/students/{id}", name="admin_show_student")
+     */
+    public function showStudent($id) :Response 
+    {
+        $student = $this->getDoctrine()->getRepository(Student::class)->findOneBy(['id' => $id]);
+
+        return $this->render('showStudent.hmtl.twig', [
+            'student' => $student,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/students/add", name="admin_add_student")
+     */
+    public function addStudent() :Response
+    {
+        $student = new Student;
+
+        //TODO form add student
+        $form = '';
+
+        return $this->render('addStudent.html.twig', [
+            //'addStudentForm' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/admin/students/mod/{id}", name="admin_show_student")
+     */
+    public function modSudent($id) :Response
+    {
+        $student = $this->getDoctrine()->getRepository(Student::class)->findOneBy(['id'=>$id]);
+        // TODO student mod form
+        $form = '';
+
+        return $this->render('modStudent.html.twig', [
+            'student' => $student,
+            //'modStudentForm' => $form->createview(),
+        ]);
+    }
+
+    public function delStudent($id) :Response
+    {
+        $student = $this->getDoctrine()->getRepository(Student::class)->findOneBy(['id'=>$id]);
+        //TODO student delete form
+        $form = '';
+
+        return $this->render('delStudent.html.twig', [
+            'student' => $student,
+            //'delStudentForm' => $form->createView(),
+        ]);
+    }
 
 
+    //TODO promotion links methods (and forms)
 }
 
 
